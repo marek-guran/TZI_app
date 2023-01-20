a91a369967ca2607c8acc892904bc51d346c1c08
Marek Guráň <skemgi2@gmail.com>
2023-01-20 14:44:15 +0100

    Small changes

---
+++ app/src/main/java/com/marekguran/tzi/KonecnyAutomat7.java
@@ -0,0 +1,14 @@
+package com.marekguran.tzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KonecnyAutomat7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konecny_automat7);
+    }
+}
