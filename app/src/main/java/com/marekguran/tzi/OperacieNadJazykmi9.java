41c5ea3011585fb40c2ffaedcc073cc86b809251
Marek Guráň <skemgi2@gmail.com>
2023-01-20 15:46:30 +0100

    Preparation for another page

---
+++ app/src/main/java/com/marekguran/tzi/OperacieNadJazykmi9.java
@@ -0,0 +1,14 @@
+package com.marekguran.tzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperacieNadJazykmi9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacie_nad_jazykmi9);
+    }
+}
